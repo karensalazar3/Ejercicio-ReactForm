9434641476fd187bd7a3e88250572e3ca823f944
karensalazar3 <karensalazar0503@gmail.com>
2024-12-12 13:30:34 +0100

    Añadir estilos

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar/Navbar"
 import Home from "./components/Home/Home"
 import Reserve from "./components/Reserve/Reserve"
 import About from "./components/About/About"
+import "./App.css"
 
 const App = () => {
   return (
